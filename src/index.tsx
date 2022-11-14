32fd68a449b39286dc138fd10c35cc3014a6d873
Cristian <cristianotona@gmail.com>
2022-11-14 02:09:45 +0100

    details/css

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./infrastructure/app/App";
 import { appStore } from "./infrastructure/store/store";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
--- src/infrastructure/components/footer/footer.tsx
+++ src/infrastructure/components/footer/footer.tsx
@@ -1,9 +1,11 @@
-import styles from "../header/header.module.css";
+import styles from "../footer/footer.module.css";
 
 export function Footer() {
     return (
-        <footer className={styles.header}>
-            <address>COMIC-GAME</address>
+        <footer className={styles.footer}>
+            <address>COMIC´S GAME</address>
+            <p>Cristian Tonato</p>
+            <p></p>
         </footer>
     );
 }
